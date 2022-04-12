b3ef2e434e43b8a506622998716708c21e8659ab
Manon Tejedor <manon.tejedor@telecomnancy.eu>
2022-04-12 11:16:01 +0200

    Ajout de la page de connexion

--- Wordle/app.py
+++ Wordle/app.py
@@ -1,27 +1,21 @@
-from atexit import register
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy() #sert pour l'authentification
+from flask import Flask, session
 
 def App():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
-    db.init_app(app)
+    app.config["SESSION_PERMANENT"]=False
+    app.config["SESSION_TYPE"]="file_system"
 
     from python.pages.home import home
     from python.pages.Jeu import game
     from python.pages.Search import search
     from python.pages.authentification import authentification
-    from python.pages.connexion import connexion
+    from python.pages.login import login
     app.register_blueprint(home)
     app.register_blueprint(game)
     app.register_blueprint(search)
     app.register_blueprint(authentification)
-    app.register_blueprint(connexion)
+    app.register_blueprint(login)
     return app
 
 if __name__ == "__main__":
