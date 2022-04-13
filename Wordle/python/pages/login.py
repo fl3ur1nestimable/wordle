ffe2eb29faf2baffd4ae7f98863f29df8b76d650
Manon Tejedor <manon.tejedor@telecomnancy.eu>
2022-04-13 19:39:01 +0100

    maj login

--- Wordle/python/pages/login.py
+++ Wordle/python/pages/login.py
@@ -1,7 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect, flash
+import sqlite3
+from python.functions.Hachage import decrypt
 
 login = Blueprint('login',__name__)
 
 @login.route('/login')
 def oui():
     return render_template('login.html')
+
+@login.route('/login', methods=['POST'])
+def connect():
+    form = request.form.to_dict()
+    email_enter=form['email']
+    mdp_enter=form['password']
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute(""" SELECT mdp FROM users WHERE mail = ?;""",(email_enter))
+    mdp_crypt = cursor.fetchall()
+    db.commit()
+    db.close()
+    
+    if decrypt(mdp_crypt[0])!=mdp_enter :
+        flash("Oups, le mail ou mot de passe est éronné")
+        return redirect('/login')
+        
+
+    return redirect('home')
