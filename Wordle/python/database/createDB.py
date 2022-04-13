74986b7a826109183edde3b206ad4bc1456ddc0f
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-04-13 14:25:48 +0200

    MAJ DB + correction

--- Wordle/python/database/createDB.py
+++ Wordle/python/database/createDB.py
@@ -1,12 +1,25 @@
 import sqlite3
 
 def createDBusers():
-    db = sqlite3.connect('database.db')
+    db = sqlite3.connect('Wordle/database.db')
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE users(id_user INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,mail TEXT, mdp TEXT) ")
+    cursor.executescript(""" DROP TABLE IF EXISTS users;
+                    CREATE TABLE users(id_user INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,mail TEXT, mdp TEXT); """)
+    db.commit()
+    db.close()
+
+
+def createDBamis():
+    query="""DROP TABLE IF EXISTS Amis;
+          CREATE TABLE Amis(id INTEGER Primary KEY,user1 TEXT, user2 TEXT, FOREIGN KEY(user1) REFERENCES users(name),FOREIGN KEY(user2) REFERENCES users(name));
+          """
+    db=sqlite3.connect("Wordle/database.db")
+    cursor=db.cursor()
+    cursor.executescript(query)
     db.commit()
     db.close()
 
 
 createDBusers()
+createDBamis()
 
