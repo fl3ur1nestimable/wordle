5fe120593dc43fdf46fef2fee1009fb4ec675c2f
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-04-13 14:55:16 +0200

    Modif page de jeu

--- Wordle/python/pages/Jeu.py
+++ Wordle/python/pages/Jeu.py
@@ -9,3 +9,8 @@ def Motus():
             return render_template('games.html')
         if methods=="GET":
             return render_template("game.html")
+
+
+@game.route('/wordle')
+def wordle():
+    return render_template('wordle.html',essais =6, longueur =5)
