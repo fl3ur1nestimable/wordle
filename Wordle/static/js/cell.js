c3cdaa0969963c756884a9520688ab99930a5c82
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2022-04-14 12:24:51 +0000

    Replace cell.js

--- Wordle/static/js/cell.js
+++ Wordle/static/js/cell.js
@@ -1,3 +1,4 @@
+
 class cell {
     constructor(letter = "", state = 0) {
         this.letter = letter;
@@ -10,17 +11,50 @@ class cell {
 
     show(){
         rectMode(CENTER);
+        this.updateColor();
         fill(this.col);
-        stroke(100);
-        strokeWeight(1.5);
+        if (this.state==0){
+            stroke(100);
+            strokeWeight(1.5);
+        }else{
+            stroke(this.col);
+            strokeWeight(1.5)
+        }
         rect(this.x ,this.y,this.w,this.w,2);
         
         fill(255);
         stroke(0);
-        strokeWeight(0.5);
+        strokeWeight(2);
         textAlign(CENTER);
-        textSize(15);
-        text(this.letter,this.x,this.y);
+        textSize(height*0.06);
+        text(this.letter,this.x,this.y+height*0.0217);
         
     }
+
+    getLetter(){
+        return this.letter;
+    }
+
+    updateColor(){
+        switch(this.state){
+            case 0 :
+                this.col = color(40,40,40);
+                break;
+            
+            case 1 :
+                this.col = color(235,200,120);
+                break;
+            
+            case 2 :
+                this.col = color(150,235,120);
+                break;
+            
+        }
+    }
 }
+
+
+//let backgroundColor = color(40,40,40);
+//let yellowColor = color(235,200,120);
+//let greenColor = color(150,235,120);
+//let alreadyUsedColor = color(100,100,100);
