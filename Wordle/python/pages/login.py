6d2b0f620bc4d0a53707bd81dbfad49f0dde5afc
Manon Tejedor <manon.tejedor@telecomnancy.eu>
2022-04-14 12:21:31 +0100

    maj login 2

--- Wordle/python/pages/login.py
+++ Wordle/python/pages/login.py
@@ -1,4 +1,5 @@
-from flask import Flask, Blueprint, render_template, request, redirect, flash
+from crypt import methods
+from flask import Flask, Blueprint, render_template, request, redirect, flash, session
 import sqlite3
 from python.functions.Hachage import decrypt
 
@@ -8,21 +9,23 @@ login = Blueprint('login',__name__)
 def oui():
     return render_template('login.html')
 
-@login.route('/login', methods=['POST'])
+@login.route('/login', methods=['POST', 'GET'])
 def connect():
-    form = request.form.to_dict()
-    email_enter=form['email']
-    mdp_enter=form['password']
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    cursor.execute(""" SELECT mdp FROM users WHERE mail = ?;""",(email_enter))
-    mdp_crypt = cursor.fetchall()
-    db.commit()
-    db.close()
+    if methods=='POST':
+        form = request.form.to_dict()
+        email_enter=form['email']
+        mdp_enter=form['password']
+        db = sqlite3.connect('database.db')
+        cursor = db.cursor()
+        cursor.execute(""" SELECT mdp, name FROM users WHERE mail = ?;""",(email_enter))
+        mdp_crypt = cursor.fetchall()
+        db.commit()
+        db.close()
     
-    if decrypt(mdp_crypt[0])!=mdp_enter :
-        flash("Oups, le mail ou mot de passe est éronné")
-        return redirect('/login')
-        
+        if decrypt(mdp_crypt[0])!=mdp_enter :
+            flash("Oups, le mail ou mot de passe est éronné")
+            return redirect('/login', nom_utilisateur= mdp_crypt[1])
 
-    return redirect('home')
+        return redirect('home')
+    
+    return render_template("login.html")
