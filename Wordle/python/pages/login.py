1fdbfbec787dbe896b5d9613119cb26f66c3177d
Manon Tejedor <manon.tejedor@telecomnancy.eu>
2022-04-14 12:23:46 +0100

    maj login 3

--- Wordle/python/pages/login.py
+++ Wordle/python/pages/login.py
@@ -17,14 +17,14 @@ def connect():
         mdp_enter=form['password']
         db = sqlite3.connect('database.db')
         cursor = db.cursor()
-        cursor.execute(""" SELECT mdp, name FROM users WHERE mail = ?;""",(email_enter))
+        cursor.execute(""" SELECT mdp FROM users WHERE mail = ?;""",(email_enter))
         mdp_crypt = cursor.fetchall()
         db.commit()
         db.close()
     
         if decrypt(mdp_crypt[0])!=mdp_enter :
             flash("Oups, le mail ou mot de passe est éronné")
-            return redirect('/login', nom_utilisateur= mdp_crypt[1])
+            return redirect('/login')
 
         return redirect('home')
     
