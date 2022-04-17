279c738014c48a2b41ff36c42bd9f1c219b8107a
TEJEDOR Manon <manon.tejedor@telecomnancy.eu>
2022-04-17 11:43:19 +0100

    page mes amis

--- Wordle/python/pages/my_friends.py
+++ Wordle/python/pages/my_friends.py
@@ -1,7 +1,19 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, session
+import sqlite3
 
 my_friends = Blueprint('my_friends',__name__)
 
 @my_friends.route('/my_friends')
 def add_friends():
-    return render_template('my_friends.html')
+    user = str(session.get("user_mail"))
+    db = sqlite3.connect("Wordle/database.db")
+    cursor= db.cursor()
+    cursor2= db.cursor()
+    query= '''SELECT user1 FROM Amis WHERE user2=? AND demand = accepted''',(user,)
+    query2 = '''SELECT user2 FROM Amis WHERE user1 =? AND demand = accepted''',(user,)
+    cursor.execute(query)
+    cursor2.execute(query2)
+    L=cursor.fetchall()
+    L2=cursor2.fetchall()
+    db.close()
+    return render_template('my_friends.html',data1=L,data2=L2)
