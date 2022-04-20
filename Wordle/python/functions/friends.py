c700801445d9eda3f1bae45c0ea46fd362459a53
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-04-20 18:47:26 +0200

    Ajout des fonctions pour accepter/refuser une demande d'ami

--- Wordle/python/functions/friends.py
+++ Wordle/python/functions/friends.py
@@ -1,9 +1,23 @@
 import sqlite3
 
-def demandes(user):
+def demande(user):
     db = sqlite3.connect('Wordle/database.db')
     cursor = db.cursor()
     cursor.execute(""" SELECT user2 FROM Amis WHERE demands=not_accepted AND user1=?""",(user,))
     l=cursor.fetchall()
     db.close()
     return l
+
+def accepte(user1,user2):
+    db = sqlite3.connect('Wordle/database.db')
+    cursor = db.cursor()
+    cursor.execute(""" UPDATE Amis SET demands=accepted WHERE user1=? AND user2=?;""",(user1,user2))
+    db.commit()
+    db.close()
+
+def refuse(user1,user2):
+    db = sqlite3.connect('Wordle/database.db')
+    cursor = db.cursor()
+    cursor.execute(""" DELETE FROM Amis WHERE user1=? AND user2=?;""",(user1,user2))
+    db.commit()
+    db.close()
--- Wordle/python/pages/demands.py
+++ Wordle/python/pages/demands.py
@@ -1,12 +1,27 @@
-from flask import Flask, Blueprint, render_template, request, redirect, session
+from flask import Flask, Blueprint, render_template, request, redirect, session, url_for
 import sqlite3
-from python.functions.friends import demandes
+from python.functions.friends import demande,accepte,refuse
 
 demands = Blueprint('demands',__name__)
 
 @demands.route('/demands')
 def show():
     user = session.get('name')
-    data = demandes(user)
+    data = demande(user)
+    return render_template('demands.html', data=data)
 
 
+@demands.route('/accepter/<user>')
+def accepter(user):
+    user1 = session.get('name')
+    user2 = user
+    accepte(user1,user2)
+    return url_for('show')
+
+
+@demands.route('/refuser/<user>')
+def refuser(user):
+    user1 = session.get('name')
+    user2 = user
+    refuse(user1,user2)
+    return url_for('show')
