df1e3a71f8e4863eeb6425dfe48a83cf16c279c9
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-04-25 16:13:34 +0200

    Ajouts des test_login

--- Wordle/python/pages/my_friends.py
+++ Wordle/python/pages/my_friends.py
@@ -3,34 +3,47 @@ import sqlite3
 from python.functions.search import liste_users_minus_friends, recherche
 from python.functions.friends import ajoute, liste_amis
 from python.functions.classement import classe_amis
+from python.functions.is_connected import test_login
 
 my_friends = Blueprint('my_friends',__name__)
 
 @my_friends.route('/my_friends')
 def show_friends():
-    user = str(session.get("name"))
-    l = classe_amis(user)
-    n = len(l)
-    return render_template('my_friends.html',data=l,n=n)
+    if test_login :
+        user = str(session.get("name"))
+        l = classe_amis(user)
+        n = len(l)
+        return render_template('my_friends.html',data=l,n=n)
+    else:
+        return redirect('/login')
 
 
 @my_friends.route('/my_friends/add',)
 def search_friends():
-    return render_template('add_friends.html')
+    if test_login :
+        return render_template('add_friends.html')
+    else:
+        return redirect('/login')
 
 @my_friends.route('/my_friends/add',methods=['POST'])
 def add_friends():
-    search = request.form.get('search')
-    user = session.get('name')
-    l = liste_users_minus_friends(user)
-    l.remove((user,))
-    users_found = recherche(search,l)
-    empty = (users_found==[])
-    return render_template('add_friends.html',users_found=users_found,empty=empty)
+    if test_login:
+        search = request.form.get('search')
+        user = session.get('name')
+        l = liste_users_minus_friends(user)
+        l.remove((user,))
+        users_found = recherche(search,l)
+        empty = (users_found==[])
+        return render_template('add_friends.html',users_found=users_found,empty=empty)
+    else:
+        return redirect('/login')
 
 
 @my_friends.route('/ajouter/<user>')
 def add(user):
-    user1,user2 = session.get('name'),user
-    ajoute(user1,user2)
-    return redirect(url_for('my_friends.add_friends'))
+    if test_login:
+        user1,user2 = session.get('name'),user
+        ajoute(user1,user2)
+        return redirect(url_for('my_friends.add_friends'))
+    else:
+        return redirect('/login')
