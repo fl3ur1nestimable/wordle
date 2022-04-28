200a93fd2e658edd1e260d1a1da5bde9519d7392
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-04-28 18:50:56 +0200

    Protection urls

--- Wordle/python/pages/historique.py
+++ Wordle/python/pages/historique.py
@@ -1,14 +1,18 @@
-from flask import Flask,Blueprint,render_template,redirect,session
+from flask import Flask,Blueprint,render_template,redirect,session,flash
 from python.functions.game import getDataGame
+from python.functions.is_connected import test_login
 
 historique = Blueprint('historique',__name__)
 
 @historique.route('/historique')
 def show_histo():
-    id_user = session.get('id')
-    data = getDataGame(id_user)
-    empty = False
-    if data == None:
-        empty = True
-    print(empty)
-    return render_template('historique.html',data=data,empty=empty)
+    if test_login():
+        id_user = session.get('id')
+        data = getDataGame(id_user)
+        empty = False
+        if data == None:
+            empty = True
+        return render_template('historique.html',data=data,empty=empty)
+    else:
+        flash('Veuillez vous connecter pour profiter de toutes les fonctionnalités ')
+        return redirect('/login')
