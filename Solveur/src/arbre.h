01998d35549454112690bd24e02c434bfc4e0a76
Alexandre <alexandre.theisse@telecomnancy.eu>
2022-05-07 12:29:17 +0200

    mise en place fonctions

--- Solveur/src/arbre.h
+++ Solveur/src/arbre.h
@@ -1,6 +1,7 @@
 #ifndef ARBRE_H
 #define ARBRE_H
-
+#include "pattern.h"
+#include "mot.h"
 typedef struct {
     char etiquette;
     struct noeud *liste_fils;
@@ -13,3 +14,11 @@ typedef struct {
 
 
 #endif /* ARBRE_H */
+
+double proba(arbre_mots one_arbre,char* mot, pattern pat);
+
+arbre_mots arbre_init(int taille);
+
+arbre_mots* arbre_update(arbre_mots one_arbre, mot mot, pattern pat);
+
+int taille_arbre(arbre_mots one_arbre);
