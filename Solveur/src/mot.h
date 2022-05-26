65fac354994e23b9e8de95cae0e13f5354e03041
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-05-26 15:46:15 +0200

    Première approche structure mot

--- Solveur/src/mot.h
+++ Solveur/src/mot.h
@@ -1,16 +1,48 @@
+#include "pattern.h"
+#include <stdbool.h>
+
 #ifndef MOT_H
 #define MOT_H
-
 
 typedef struct {
     char* val;
     double entropy;
 }mot;
 
+/*
 
+struct _noeud{
+    char etiquette;
+    struct _liste *liste_fils;
+    struct _noeud *next;
+};
+typedef struct _noeud noeud;
 
-mot mot_generate_best(arbre_mots one_arbre);
+struct _liste{
+    struct _noeud *head;
+};
+typedef struct _liste liste;
 
-char* mot_get_val(mot mot);
+typedef struct {
+    noeud *root;
+}arbre_mots;
+
+*/
+
+mot *mot_create(char *str);
+
+void mot_destroy();
+
+bool noeud_is_empty(noeud *one_noeud);
+
+void mot_arbre_explore(noeud *one_noeud, char* str, mot *best);
+
+bool noeud_is_last(noeud *one_noeud);
+
+void mot_generate_best(arbre_mots *one_arbre);
+
+char* mot_get_val(mot *mot);
+
+double mot_entropy(double *list_entro);
 
 #endif /* MOT_H */
