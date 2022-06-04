0f6b9077680a244df5e3312a8dcf3a79e7e2f53b
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-06-04 13:23:34 +0200

    Ajout du destroy liste pattern

--- Solveur/src/pattern.h
+++ Solveur/src/pattern.h
@@ -10,15 +10,17 @@
 
 pattern** pattern_init_all(int taille );
 
-double entropy_pat(pattern* one_pattern, char * mot,arbre_mots *arbre);
+double entropy_pat(pattern* one_pattern, mot* m,arbre_mots *arbre);
 
-double moy_entropy(pattern** patterns, char* mot, arbre_mots *arbre,int taille);
+double moy_entropy(pattern** patterns, mot* m, arbre_mots *arbre,int taille);
 
 pattern *pattern_from_input(int* tab,int taille);
 
 void pattern_print(pattern *one_pattern);
 
 void pattern_destroy(pattern* one_pattern);
+
+void patterns_destroy(pattern **patterns);
 
 long base3(int n);
 
