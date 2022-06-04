382c3c2b5ab4f97d4fb935ed3ff935c1a57fb4db
Coralie Serrand <coralie.serrand@telecomnancy.eu>
2022-06-04 13:21:43 +0200

    Supression des fonctions inutiles, ajout des fonctions récursives sur les noeuds

--- Solveur/src/arbre.h
+++ Solveur/src/arbre.h
@@ -12,15 +12,13 @@ noeud* noeud_init(char val,noeud *prev,noeud *parent);
 void lecture_fichier(arbre_mots* arbre, int n);
 void arbre_append_mot(arbre_mots* arbre, char* m);
 
-bool liste_remove_noeud(liste* l, noeud *current);
 void arbre_update(arbre_mots* one_arbre, mot* m, pattern *pat);
-void liste_update(liste *l,mot *m, pattern *pat,int depth,char* str);
 void noeud_update(arbre_mots *arbre, noeud *node,mot *m, pattern* pat, int depth,char* str);
-void remove_liste(liste* l);
+
 void noeud_remove(arbre_mots *arbre,noeud *node);
 
-int taille_arbre(arbre_mots *one_arbre);
-int taille_liste(liste* l);
+int arbre_taille(arbre_mots *one_arbre);
+int noeud_taille(noeud* node);
 
 void arbre_destroy(arbre_mots *arbre);
 void noeud_destroy(noeud *node);
@@ -30,8 +28,8 @@ void printNTree(noeud* n, bool flag[], int depth, bool isLast);
 
 void arbre_init_nb_mots(arbre_mots *arbre);
 int noeud_init_nb_mots(noeud *node);
-double proba(arbre_mots* arbre, mot* mot, pattern* one_pattern);
-int nb_mots_pat(liste *l,mot *m, pattern *pat,int depth,char* str);
+
+double arbre_proba(arbre_mots* arbre, mot *m, pattern* one_pattern);
 int noeud_nb_mot_coupe(noeud *node,mot *m,pattern *pat,int depth,char *str);
 int arbre_nb_mot(arbre_mots *arbre, mot *m, pattern *pat);
 
