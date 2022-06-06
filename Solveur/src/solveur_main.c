299354b83833ac9640b2c5ac321bb0e014b8335b
YEBOUET <antoine.yebouet@telecomnancy.eu>
2022-06-06 15:46:55 +0200

    changes stdin & comments

--- Solveur/src/solveur_main.c
+++ Solveur/src/solveur_main.c
@@ -41,8 +41,6 @@ int main(){
     printf("Entrez le nombre d'essais : \n");
     scanf("%d",&nb_essais);
     int guess[taille];
-    int power = pow(3,taille);
-    int possibilites[power];
     bool not_win = true;
     int i = nb_essais;
 
@@ -50,15 +48,15 @@ int main(){
     printf("Vos réponse doivent être de la forme xxx où il y'a autant de x que la taille du mot et où x vaut 0,1, ou 2 \n");
     printf("0 sont les lettres n'étant pas dans le mot, 2 les lettres bien placées et 1 les lettres mal placées \n");
     while(i!=0 && not_win){
-        char* mot_propose = "oui"; // cela sera égal à la fonction qui renvoit la meilleure proposition de mots
-        printf("%s",mot_propose);
+        char* mot_propose = "oui"; //generate best mot ici
+        printf("Mot proposé : %s\n",mot_propose);
         int somme = 2*taille;
         int result;
         printf("Entrez le résultat pour le mot : \n");
         scanf("%d",&result);
 
         if(result==-1){
-            printf("Arrêt");
+            printf("Arrêt\n");
             return 0;
         }
         
@@ -66,7 +64,7 @@ int main(){
             printf("Votre mot n'est pas dans les possibilités : \nEntrez le résultat pour le mot : \n");
             scanf("%d",&result);
             if(result==-1){
-                printf("Arrêt");
+                printf("Arrêt\n");
                 return 0;
             }
         }
@@ -79,19 +77,20 @@ int main(){
             a=a-guess[j-1]*pow(10,taille-j);
             guess[j]=a*pow(10,-(taille-1-j));
             somme = somme - guess[j];
-            printf("%d \n",guess[j]);
         }
-
+        pattern * input = pattern_from_input(guess,taille);
+        pattern_print(input);
         // il faut envoyer le tableau guess a la fonction
-
+        //arbre update et tout le tralala ici
+        pattern_destroy(input);
         i=i-1;
 
         if(somme == 0){
-            printf(" Trouvé ! ");
+            printf("Trouvé !\n");
             not_win = false;
         }
         else{
-            printf("Sorry");
+            printf("Mot non trouvé\n");
         }
     }
     return 0;  
